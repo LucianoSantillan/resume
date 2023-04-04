b0696bb794c8a606881ba4ee7e797ac3a6317a51
Luciano Santillan <lucianosantillan96@outlook.com.ar>
2023-04-04 11:12:47 +0200

    refactor: Skills and experience

---
+++ pages/components/SkillsAndExperience/SkillsAndExperience.tsx
@@ -0,0 +1,77 @@
+import styles from './SkillsAndExperience.module.css';
+import { Box, NoSsr, Tab, Tabs, Typography } from '@mui/material';
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import SkillsTab from '../SkillsTab/SkillsTab';
+import VerticalTabs from '../ExperienceTab/ExperienceTab';
+
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+function a11yProps(index: number) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
+export default function SkillsAndExperience() {
+
+    const [tab, setTab] = useState<number>(1)
+
+    return (
+
+        <div id='skills-and-experience-section' className={styles.skillsAndExperience}>
+            <div className={styles.container}>
+                <div className={styles.tabsContainer}>
+                    <Tabs value={tab} onChange={(_, value) => { console.log(value); setTab(value) }}>
+                        <Tab label="SKILLS" {...a11yProps(0)} />
+                        <Tab label="EXPERIENCE" {...a11yProps(1)} />
+                    </Tabs>
+                </div>
+                <TabPanel value={tab} index={0}>
+                    <SkillsTab />
+                </TabPanel>
+                <TabPanel value={tab} index={1}>
+                    <NoSsr>
+                        <VerticalTabs />
+                    </NoSsr>
+                </TabPanel>
+            </div>
+            <Button
+                variant="contained"
+                startIcon={<LinkedInIcon />}
+                href="https://www.linkedin.com/in/luciano-santill%C3%A1n-b11ab4178/"
+                target="_blank"
+                rel="noopener"
+                className={styles.linkedInButton}
+            >
+                LinkedIn
+            </Button>
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,13 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
-import { Box, IconButton, NoSsr, Tab, Tabs, Typography } from '@mui/material';
 import Banner from './components/Banner/Banner';
-import About from './components/About/About';
-import SkillsTab from './components/SkillsTab/SkillsTab';
-import { useState } from 'react';
-import VerticalTabs from './components/ExperienceTab/ExperienceTab';
-import Button from '@mui/material/Button';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import About from './components/About/About';;
 import Navbar from './components/Navbar/Navbar';
 import Form from './components/ContactForm/ContactForm';
 import Footer from './components/Footer/Footer';
-
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
-
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-function a11yProps(index: number) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
+import SkillsAndExperience from './components/SkillsAndExperience/SkillsAndExperience'
 
 export default function Home() {
-
-  const [tab, setTab] = useState<number>(1)
 
   return (
     <>
@@ -61,44 +21,7 @@ export default function Home() {
         <Navbar></Navbar>
         <Banner />
         <About />
-        <div id='skills-and-experience-section' className={styles.skillsAndExperience}>
-          <Box sx={{
-            width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center'
-          }}>
-            <Box sx={{ borderBottom: 1, borderColor: 'divider', maxWidth: '870px' }}>
-              <Tabs value={tab} onChange={(_, value) => { console.log(value); setTab(value) }}>
-                <Tab label="SKILLS" {...a11yProps(0)} />
-                <Tab label="EXPERIENCE" {...a11yProps(1)} />
-                {/* <Tab label="Item Three" {...a11yProps(2)} /> */}
-              </Tabs>
-            </Box>
-            <TabPanel value={tab} index={0}>
-              <SkillsTab />
-            </TabPanel>
-            <TabPanel value={tab} index={1}>
-              <NoSsr>
-                <VerticalTabs />
-              </NoSsr>
-            </TabPanel>
-            {/* <TabPanel value={value} index={1}>
-            Item Two
-          </TabPanel>
-          <TabPanel value={value} index={2}>
-            Item Three
-          </TabPanel> */}
-          </Box>
-          <Button
-            variant="contained"
-            startIcon={<LinkedInIcon />}
-            href="https://www.linkedin.com/in/luciano-santill%C3%A1n-b11ab4178/"
-            target="_blank"
-            rel="noopener"
-            className={styles.linkedInButton}
-          >
-            LinkedIn
-          </Button>
-          );
-        </div>
+        <SkillsAndExperience />
         <Form></Form>
         <Footer />
       </main>
