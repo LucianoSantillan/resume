ca45d7f81428e210855cb1ec5ef00101e79a6ad7
Luciano Santillan <lucianosantillan96@outlook.com.ar>
2023-04-05 16:18:14 +0200

    banner responsive

--- pages/components/Banner/Banner.tsx
+++ pages/components/Banner/Banner.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Banner.module.css'
+import styles from './Banner.module.css';
 import Button from '@mui/material/Button';
 import { scroller as scroll } from 'react-scroll';
 
@@ -12,20 +12,20 @@ const handleClick = () => {
 };
 
 export default function Banner() {
-    return (
-        <div className={styles.banner}>
-            <div className={styles.bannerContent}>
-                <div className={styles.shapeOuterChevronRight}>
-                    <div className={styles.shapeInnerChevronRight}></div>
-                </div>
-                <div className={styles.bannerTextAndBtn}>
-                    <div className={styles.sayHi}> Hi! My name is Luciano</div>
-                    <div className={styles.whoIAm}>
-                        I’m a Frontend Developer, specialized in development of web applications using React, Next.js, and more!
-                    </div>
-                    <Button onClick={handleClick} variant="contained">FIND OUT MORE</Button>
-                </div>
-            </div>
+  return (
+    <div className={styles.banner}>
+      <div className={styles.bannerContent}>
+        <div className={styles.shapeOuterChevronRight}>
+          <div className={styles.shapeInnerChevronRight}></div>
         </div>
-    )
+        <div className={styles.bannerTextAndBtn}>
+          <div className={styles.sayHi}> Hi! My name is Luciano</div>
+          <div className={styles.whoIAm}>
+            I’m a Frontend Developer, specialized in development of web applications using React, Next.js, and more!
+          </div>
+          <Button onClick={handleClick} variant="contained">FIND OUT MORE</Button>
+        </div>
+      </div>
+    </div>
+  )
 }
