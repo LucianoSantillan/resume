2c678160f59d5e90163bad2dff08dcd234e16875
Luciano Santillan <lucianosantillan96@outlook.com.ar>
2023-04-05 16:41:05 +0200

    navbar responsive

--- pages/components/Navbar/Navbar.tsx
+++ pages/components/Navbar/Navbar.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import { AppBar, Button, Toolbar } from '@mui/material';
+import React, { useState } from 'react';
+import { AppBar, Button, Toolbar, IconButton } from '@mui/material';
 import { scroller } from 'react-scroll';
 import { ABOUT_SECTION_ID } from '../About/About';
 import { SKILLS_AND_EXPERIENCE_SECTION_ID } from '../SkillsAndExperience/SkillsAndExperience';
 import { CONTACT_SECTION_ID } from '../ContactForm/ContactForm';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import styles from './Navbar.module.css';
 
 const handleClick = (elementId: string) => {
   scroller.scrollTo(elementId, {
@@ -14,15 +17,44 @@ const handleClick = (elementId: string) => {
 };
 
 function Navbar() {
+  const [burgerMenuOpen, setBurgerMenuOpen] = useState(false);
+
+  const handleBurgerMenuClick = () => {
+    setBurgerMenuOpen(!burgerMenuOpen);
+  };
+
+  const handleNavLinkClick = (elementId: string) => {
+    setBurgerMenuOpen(false);
+    handleClick(elementId);
+  };
+
   return (
     <AppBar position="fixed" sx={{ backgroundColor: "#fff", color: 'black', top: 0 }}>
-      <Toolbar style={{ justifyContent: 'center' }}>
-        <Button onClick={() => handleClick(ABOUT_SECTION_ID)} color="inherit">About</Button>
-        <Button onClick={() => handleClick(SKILLS_AND_EXPERIENCE_SECTION_ID)} color="inherit">Experience</Button>
-        <Button onClick={() => handleClick(CONTACT_SECTION_ID)} color="inherit">Contact</Button>
+      <Toolbar>
+        <div className={styles.navbar}>
+        <Button onClick={() => handleNavLinkClick(ABOUT_SECTION_ID)} color="inherit">About</Button>
+          <Button onClick={() => handleNavLinkClick(SKILLS_AND_EXPERIENCE_SECTION_ID)} color="inherit">Experience</Button>
+          <Button onClick={() => handleNavLinkClick(CONTACT_SECTION_ID)} color="inherit">Contact</Button>
+        </div>
+        <IconButton
+          edge="end"
+          className={styles.burgerMenu}
+          color="inherit"
+          onClick={handleBurgerMenuClick}
+        >
+          {burgerMenuOpen ? <CloseIcon /> : <MenuIcon />}
+        </IconButton>
       </Toolbar>
+      {burgerMenuOpen && (
+        <Toolbar>
+          <Button onClick={() => handleNavLinkClick(ABOUT_SECTION_ID)} color="inherit">About</Button>
+          <Button onClick={() => handleNavLinkClick(SKILLS_AND_EXPERIENCE_SECTION_ID)} color="inherit">Experience</Button>
+          <Button onClick={() => handleNavLinkClick(CONTACT_SECTION_ID)} color="inherit">Contact</Button>
+        </Toolbar>
+      )}
     </AppBar>
   );
 }
 
 export default Navbar;
+
