c7fd0c918ed84a29c1f7291d8953fc082e7a1e6e
Luciano Santillan <lucianosantillan96@outlook.com.ar>
2023-06-01 17:50:18 +0200

    feat: page loading transition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Router from "next/router"
+
+import styles from './Loading.module.css';
+
+function Loading() {
+  return (
+    <div className={styles.loading}>
+      <div className={styles.spinner}></div>
+    </div>
+  );
+}
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    // Used for page transition
+    const start = () => {
+      setLoading(true)
+    }
+    const end = () => {
+      setLoading(false)
+    }
+    Router.events.on("routeChangeStart", start)
+    Router.events.on("routeChangeComplete", end)
+    Router.events.on("routeChangeError", end)
+    return () => {
+      Router.events.off("routeChangeStart", start)
+      Router.events.off("routeChangeComplete", end)
+      Router.events.off("routeChangeError", end)
+    }
+  }, [])
+
+  return loading ? <Loading /> : <Component {...pageProps} />
 }
