e76588bd78a3fd9b3bae1353c8f3a72628e5f557
Luciano Santillan <lucianosantillan96@outlook.com.ar>
2023-07-19 19:51:14 +0200

    feat: article page

---
+++ pages/articles/[id].tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import { Article } from '.';
+import { useRouter } from 'next/router';
+import styles from './Article.module.css'
+import Navbar from '@/components/Navbar/Navbar';
+
+const Page: React.FC = () => {
+    const [article, setArticle] = useState<Article | null>(null);
+
+    const router = useRouter();
+    const { id } = router.query;
+
+    useEffect(() => {
+        const fetchArticle = async () => {
+            const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+            const response = await fetch(`${apiUrl}/articles/${id}`);
+            const data = await response.json();
+            setArticle(data);
+        };
+
+        if (id) {
+            fetchArticle();
+        }
+    }, [id]);
+
+    if (!article) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div className={styles.container}>
+            <Navbar />
+            <div className={styles.content}>
+                <h1 className={styles.title}>{article.title}</h1>
+                <p className={styles.description}>{article.description}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Page;
