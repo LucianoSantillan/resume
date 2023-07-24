5306595f7ecf61a4bcdab4082b89f147e4b3b263
Luciano Santillan <lucianosantillan96@outlook.com.ar>
2023-07-24 19:19:53 +0200

    fix: pagination

--- services/articles.service.ts
+++ services/articles.service.ts
@@ -2,6 +2,6 @@ import { Article } from "@/domain/models";
 import api from "@/utils/api";
 
 export const getArticles = async (page: number, pageSize: number): Promise<{ articles: Article[], totalPages: number }> => {
-  const response = await api.get("/articles?page=${page}&pageSize=${pageSize}")
+  const response = await api.get(`/articles?page=${page}&pageSize=${pageSize}`)
   return response.data
 };
